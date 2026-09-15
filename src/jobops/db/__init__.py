3d02760fa907bbacd6c88f7c6a30d8bd89f74fce
joshuaokent-spec <joshua.o.kent@gmail.com>
2026-09-15 15:51:44 -0400

    feat: export approval persistence

--- src/jobops/db/__init__.py
+++ src/jobops/db/__init__.py
@@ -1,12 +1,16 @@
+from jobops.db.approval_repository import ApprovalRepository, SqlAlchemyApprovalRepository
 from jobops.db.base import Base
-from jobops.db.models import JobRecord
+from jobops.db.models import ApprovalRecord, JobRecord
 from jobops.db.repositories import JobRepository, SqlAlchemyJobRepository
 from jobops.db.session import build_engine, build_session_factory, session_scope
 
 __all__ = [
+    "ApprovalRecord",
+    "ApprovalRepository",
     "Base",
     "JobRecord",
     "JobRepository",
+    "SqlAlchemyApprovalRepository",
     "SqlAlchemyJobRepository",
     "build_engine",
     "build_session_factory",
