3bda9f38116a01249d9ed124461905ebd6177540
joshuaokent-spec <joshua.o.kent@gmail.com>
2026-09-21 15:05:32 -0400

    feat: export search profile persistence

--- src/jobops/db/__init__.py
+++ src/jobops/db/__init__.py
@@ -5,10 +5,15 @@ from jobops.db.models import (
     ApprovalRecord,
     FeedbackEventRecord,
     JobRecord,
+    SearchProfileRecord,
     SubmissionAttemptRecord,
     SubmitAuthorizationRecord,
 )
 from jobops.db.repositories import JobRepository, SqlAlchemyJobRepository
+from jobops.db.search_profile_repository import (
+    SearchProfileRepository,
+    SqlAlchemySearchProfileRepository,
+)
 from jobops.db.session import build_engine, build_session_factory, session_scope
 from jobops.db.submission_repository import SqlAlchemySubmissionRepository
 
@@ -20,10 +25,13 @@ __all__ = [
     "FeedbackRepository",
     "JobRecord",
     "JobRepository",
+    "SearchProfileRecord",
+    "SearchProfileRepository",
     "SubmissionAttemptRecord",
     "SqlAlchemyApprovalRepository",
     "SqlAlchemyFeedbackRepository",
     "SqlAlchemyJobRepository",
+    "SqlAlchemySearchProfileRepository",
     "SqlAlchemySubmissionRepository",
     "SubmitAuthorizationRecord",
     "build_engine",
