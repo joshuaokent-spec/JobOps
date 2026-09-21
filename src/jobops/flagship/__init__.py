b21cf7ed2b2e7fdf51abd80202f5a0295c616b8c
joshuaokent-spec <joshua.o.kent@gmail.com>
2026-09-21 15:41:00 -0400

    F4: export flagship inbox service

--- src/jobops/flagship/__init__.py
+++ src/jobops/flagship/__init__.py
@@ -1,3 +1,4 @@
+from jobops.flagship.inbox import FlagshipInboxService
 from jobops.flagship.runner import FlagshipRunError, FlagshipRunService
 
-__all__ = ["FlagshipRunError", "FlagshipRunService"]
+__all__ = ["FlagshipInboxService", "FlagshipRunError", "FlagshipRunService"]
