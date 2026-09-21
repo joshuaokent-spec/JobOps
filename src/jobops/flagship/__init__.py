9609bbcd8d1f448bae1c597a1f28415b596dd430
joshuaokent-spec <joshua.o.kent@gmail.com>
2026-09-21 17:20:35 -0400

    feat: export F7 scheduling and tracking services

--- src/jobops/flagship/__init__.py
+++ src/jobops/flagship/__init__.py
@@ -1,10 +1,15 @@
 from jobops.flagship.execution import FlagshipApplicationExecutionService
 from jobops.flagship.readiness import FlagshipReadinessService
 from jobops.flagship.runner import FlagshipRunError, FlagshipRunService
+from jobops.flagship.schedule import DailyFlagshipRunner, PrivateFlagshipRunInputStore
+from jobops.flagship.tracking import FlagshipTrackingService
 
 __all__ = [
+    "DailyFlagshipRunner",
     "FlagshipApplicationExecutionService",
     "FlagshipReadinessService",
     "FlagshipRunError",
     "FlagshipRunService",
+    "FlagshipTrackingService",
+    "PrivateFlagshipRunInputStore",
 ]
